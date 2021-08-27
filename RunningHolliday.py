59757ac5e133ae147dc386958b454f46eec829f2
unknown <lukaselia.heberling@gmail.com>
2021-08-27 18:57:22 +0200

    Some formatting changes to the Code ...

--- RunningHolliday.py
+++ RunningHolliday.py
@@ -40,16 +40,53 @@ pygame.display.set_caption("RunningHolliday ~Lukas")
 
 # Some layers to create the Background
 layers = [
-    [pygame.image.load(
-        'Graphics/Background/Layers/1.png').convert_alpha(), 0.1],
-    [pygame.image.load(
-        'Graphics/Background/Layers/2.png').convert_alpha(), 0.2],
-    [pygame.image.load(
-        'Graphics/Background/Layers/3.png').convert_alpha(), 0.3],
-    [pygame.image.load(
-        'Graphics/Background/Layers/4.png').convert_alpha(), 0.4],
-    [pygame.image.load(
-        'Graphics/Background/Layers/5.png').convert_alpha(), 0.5],
+    [pygame.image.load("Graphics/Background/Layers/1.png").convert_alpha(), 0.1],
+    [pygame.image.load("Graphics/Background/Layers/2.png").convert_alpha(), 0.2],
+    [pygame.image.load("Graphics/Background/Layers/3.png").convert_alpha(), 0.3],
+    [pygame.image.load("Graphics/Background/Layers/4.png").convert_alpha(), 0.4],
+    [pygame.image.load("Graphics/Background/Layers/5.png").convert_alpha(), 0.5],
+]
+tiles = {
+    "down": pygame.image.load("Graphics/Tiles/Down.png").convert(),
+    "left": pygame.image.load("Graphics/Tiles/Left.png").convert(),
+    "right": pygame.image.load("Graphics/Tiles/Right.png").convert(),
+    "up": pygame.image.load("Graphics/Tiles/Up.png").convert(),
+    "no": pygame.image.load("Graphics/Tiles/No.png").convert(),
+    "downLeft": pygame.image.load("Graphics/Tiles/downLeft.png").convert(),
+    "downRight": pygame.image.load("Graphics/Tiles/DownRight.png").convert(),
+    "upLeft": pygame.image.load("Graphics/Tiles/UpLeft.png").convert(),
+    "upRight": pygame.image.load("Graphics/Tiles/UpRight.png").convert(),
+}
+flag = [
+    pygame.image.load("Graphics/Objects/Flag/Flag_0.png").convert_alpha(),
+    pygame.image.load("Graphics/Objects/Flag/Flag_1.png").convert_alpha(),
+    pygame.image.load("Graphics/Objects/Flag/Flag_2.png").convert_alpha(),
+    pygame.image.load("Graphics/Objects/Flag/Flag_3.png").convert_alpha(),
+]
+
+vitaRunRight = [
+    pygame.image.load("Graphics/Vita/vita_05.png").convert_alpha(),
+    pygame.image.load("Graphics/Vita/vita_06.png").convert_alpha(),
+    pygame.image.load("Graphics/Vita/vita_07.png").convert_alpha(),
+    pygame.image.load("Graphics/Vita/vita_08.png").convert_alpha(),
+    pygame.image.load("Graphics/Vita/vita_09.png").convert_alpha(),
+    pygame.image.load("Graphics/Vita/vita_10.png").convert_alpha(),
+]
+
+vitaRunLeft = [
+    pygame.image.load("Graphics/Vita/vita_05left.png").convert_alpha(),
+    pygame.image.load("Graphics/Vita/vita_06left.png").convert_alpha(),
+    pygame.image.load("Graphics/Vita/vita_07left.png").convert_alpha(),
+    pygame.image.load("Graphics/Vita/vita_08left.png").convert_alpha(),
+    pygame.image.load("Graphics/Vita/vita_09left.png").convert_alpha(),
+    pygame.image.load("Graphics/Vita/vita_10left.png").convert_alpha(),
+]
+vitaIdle = [
+    pygame.image.load("Graphics/Vita/vita_00.png").convert_alpha(),
+    pygame.image.load("Graphics/Vita/vita_01.png").convert_alpha(),
+    pygame.image.load("Graphics/Vita/vita_02.png").convert_alpha(),
+    pygame.image.load("Graphics/Vita/vita_03.png").convert_alpha(),
+    pygame.image.load("Graphics/Vita/vita_04.png").convert_alpha(),
 ]
 
 
@@ -69,10 +106,10 @@ def drawOnScreen(picture, x, y):
 
 def getGameMap(path):
     # Building the Map out of the Level.txt file
-    f = open(path + '.txt', 'r')
+    f = open(path + ".txt", "r")
     data = f.read()
     f.close()
-    data = data.split('\n')
+    data = data.split("\n")
     game_map = []
     for row in data:
         game_map.append(list(row))
@@ -80,9 +117,9 @@ def getGameMap(path):
 
 
 def menu():
-    layerX = 0
-    while (1):
-        layerX -= 1
+    layerX = -150
+    while 1:
+        layerX -= 0.5
         clicked = False
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -91,12 +128,12 @@ def menu():
                 clicked = True
         mousePos = pygame.mouse.get_pos()
         mouseRect = pygame.Rect(mousePos[0], mousePos[1], 20, 20)
-        buttonRect = pygame.Rect(300, 320, 100, 50)
+        buttonRect = pygame.Rect(300, 320, 100, 60)
         buttonSelected = False
         if mouseRect.colliderect(buttonRect):
             buttonSelected = True
         if buttonSelected:
-            buttonColor = colors["lightBlue"]
+            buttonColor = colors["black"]
         else:
             buttonColor = colors["darkGrey"]
         if clicked and buttonSelected:
@@ -111,35 +148,14 @@ def menu():
         pygame.display.update()
 
 
-class mainClass():
+class mainClass:
     def __init__(self):
         # MAP
         self.map = getGameMap("Level1")
         self.scroll = [0, 0]
         self.mapScroll = [0, 0]
-        self.tiles = {
-            "down": pygame.image.load('Graphics/Tiles/Down.png').convert(),
-            "left": pygame.image.load('Graphics/Tiles/Left.png').convert(),
-            "right": pygame.image.load('Graphics/Tiles/Right.png').convert(),
-            "up": pygame.image.load('Graphics/Tiles/Up.png').convert(),
-            "no": pygame.image.load('Graphics/Tiles/No.png').convert(),
-            "downLeft": pygame.image.load('Graphics/Tiles/downLeft.png').convert(),
-            "downRight": pygame.image.load('Graphics/Tiles/DownRight.png').convert(),
-            "upLeft": pygame.image.load('Graphics/Tiles/UpLeft.png').convert(),
-            "upRight": pygame.image.load('Graphics/Tiles/UpRight.png').convert(),
-        }
         self.flagAnimation = 0
         self.flagPosition = [2600, 175]
-        self.flag = [
-            pygame.image.load(
-                'Graphics/Objects/Flag/Flag_0.png').convert_alpha(),
-            pygame.image.load(
-                'Graphics/Objects/Flag/Flag_1.png').convert_alpha(),
-            pygame.image.load(
-                'Graphics/Objects/Flag/Flag_2.png').convert_alpha(),
-            pygame.image.load(
-                'Graphics/Objects/Flag/Flag_3.png').convert_alpha(),
-        ]
         self.tileHeight = 32
 
         # Player
@@ -158,34 +174,14 @@ class mainClass():
         # direction = [walkRight, walkLeft, Jump, stand]
         # TODO mayby add the ability to duck ...
         self.direction = [0, 0, 0, 1]
-        self.vitaRunRight = [
-            pygame.image.load('Graphics/Vita/vita_05.png').convert_alpha(),
-            pygame.image.load('Graphics/Vita/vita_06.png').convert_alpha(),
-            pygame.image.load('Graphics/Vita/vita_07.png').convert_alpha(),
-            pygame.image.load('Graphics/Vita/vita_08.png').convert_alpha(),
-            pygame.image.load('Graphics/Vita/vita_09.png').convert_alpha(),
-            pygame.image.load('Graphics/Vita/vita_10.png').convert_alpha(),
-        ]
-        self.vitaRunLeft = [
-            pygame.image.load('Graphics/Vita/vita_05left.png').convert_alpha(),
-            pygame.image.load('Graphics/Vita/vita_06left.png').convert_alpha(),
-            pygame.image.load('Graphics/Vita/vita_07left.png').convert_alpha(),
-            pygame.image.load('Graphics/Vita/vita_08left.png').convert_alpha(),
-            pygame.image.load('Graphics/Vita/vita_09left.png').convert_alpha(),
-            pygame.image.load('Graphics/Vita/vita_10left.png').convert_alpha(),
-        ]
-        self.vitaIdle = [
-            pygame.image.load('Graphics/Vita/vita_00.png').convert_alpha(),
-            pygame.image.load('Graphics/Vita/vita_01.png').convert_alpha(),
-            pygame.image.load('Graphics/Vita/vita_02.png').convert_alpha(),
-            pygame.image.load('Graphics/Vita/vita_03.png').convert_alpha(),
-            pygame.image.load('Graphics/Vita/vita_04.png').convert_alpha(),
-        ]
 
     def drawTile(self, tile, x, y):
         # Drawing the Blocks of the map
-        drawOnScreen(tile, x * self.tileHeight -
-                     self.scroll[0], y * self.tileHeight - self.scroll[1])
+        drawOnScreen(
+            tile,
+            x * self.tileHeight - self.scroll[0],
+            y * self.tileHeight - self.scroll[1],
+        )
 
     def drawLayers(self):
         # Drawing the layers of the Background
@@ -195,8 +191,8 @@ class mainClass():
     def drawMap(self):
         # Calc scroll player movement
         # Scrolling ist the Value that the tiles move so the player is standing in the middle of the screen
-        self.mapScroll[0] += (self.playerX - self.mapScroll[0]-330) / 15
-        self.mapScroll[1] += (self.playerY - self.mapScroll[1]-310) / 15
+        self.mapScroll[0] += (self.playerX - self.mapScroll[0] - 330) / 20
+        self.mapScroll[1] += (self.playerY - self.mapScroll[1] - 310) / 20
         self.scroll = self.mapScroll.copy()
         self.scroll[0] = int(self.scroll[0])
         self.scroll[1] = int(self.scroll[1])
@@ -216,45 +212,41 @@ class mainClass():
                             x * self.tileHeight - self.scroll[0],
                             y * self.tileHeight - self.scroll[1],
                             self.tileHeight,
-                            self.tileHeight
-                        )
+                            self.tileHeight,
+                        ),
                     )
                 if block == "1":
-                    self.drawTile(self.tiles["up"], x, y)
+                    self.drawTile(tiles["up"], x, y)
                 if block == "2":
-                    self.drawTile(self.tiles["down"], x, y)
+                    self.drawTile(tiles["down"], x, y)
                 if block == "3":
-                    self.drawTile(self.tiles["left"], x, y)
+                    self.drawTile(tiles["left"], x, y)
                 if block == "4":
-                    self.drawTile(self.tiles["right"], x, y)
+                    self.drawTile(tiles["right"], x, y)
                 if block == "5":
-                    self.drawTile(self.tiles["upLeft"], x, y)
+                    self.drawTile(tiles["upLeft"], x, y)
                 if block == "6":
-                    self.drawTile(self.tiles["upRight"], x, y)
+                    self.drawTile(tiles["upRight"], x, y)
                 if block == "7":
-                    self.drawTile(self.tiles["downLeft"], x, y)
+                    self.drawTile(tiles["downLeft"], x, y)
                 if block == "8":
-                    self.drawTile(self.tiles["downRight"], x, y)
+                    self.drawTile(tiles["downRight"], x, y)
                 if block == "9":
-                    self.drawTile(self.tiles["no"], x, y)
+                    self.drawTile(tiles["no"], x, y)
                 x += 1
             y += 1
         drawOnScreen(
-            self.flag[0],
+            flag[0],
             self.flagPosition[0] - self.scroll[0],
-            self.flagPosition[1] - self.scroll[1]
+            self.flagPosition[1] - self.scroll[1],
         )
 
     # PLAYER
     def checkforwin(self):
-        flagRect = pygame.Rect(
-            self.flagPosition[0],
-            self.flagPosition[1],
-            48,
-            48
-        )
+        flagRect = pygame.Rect(self.flagPosition[0], self.flagPosition[1], 48, 48)
         playerRect = pygame.Rect(
-            self.playerX, self.playerY, self.playerWidth, self.playerHeight)
+            self.playerX, self.playerY, self.playerWidth, self.playerHeight
+        )
         if playerRect.colliderect(flagRect):
             self.map = getGameMap("Level2")
             self.resetPLayer()
@@ -263,14 +255,17 @@ class mainClass():
         # Checking if the player is colliding with the world
         collision = False
         # player rect
-        playerRect = pygame.Rect(
-            newX, newY, self.playerWidth, self.playerHeight)
+        playerRect = pygame.Rect(newX, newY, self.playerWidth, self.playerHeight)
         y = 0
         for row in self.map:
             x = 0
             for block in row:
                 mapBlock = pygame.Rect(
-                    x*self.tileHeight, y*self.tileHeight, self.tileHeight, self.tileHeight)
+                    x * self.tileHeight,
+                    y * self.tileHeight,
+                    self.tileHeight,
+                    self.tileHeight,
+                )
                 if mapBlock.colliderect(playerRect) and block != "0":
                     collision = True
                 x += 1
@@ -286,9 +281,6 @@ class mainClass():
         self.playerX = 50
         self.playerY = -150
 
-    def setDirection(self, direction):
-        self.direction = direction
-
     def updatePlayer(self):
         # Getting the pressed key
         pressed = pygame.key.get_pressed()
@@ -297,7 +289,11 @@ class mainClass():
         # Set the Variables to initialize a jump
         if pressed[pygame.K_ESCAPE]:
             menu()
-        if pressed[pygame.K_UP] and not self.jump and self.collide(self.playerX, self.playerY + 1):
+        if (
+            pressed[pygame.K_UP]
+            and not self.jump
+            and self.collide(self.playerX, self.playerY + 1)
+        ):
             self.direction = [0, 0, 1, 0]
             self.jump = True
             self.jumpvar = 15
@@ -310,14 +306,19 @@ class mainClass():
             self.direction = [1, 0, 0, 0]
             self.movePlayer(self.playerX + self.speed, self.playerY)
 
-        if not self.collide(self.playerX, self.playerY + self.velocityDown) and self.jumpvar == 0:
+        if (
+            not self.collide(self.playerX, self.playerY + self.velocityDown)
+            and self.jumpvar == 0
+        ):
             self.playerY += self.velocityDown
             self.velocityDown += 0.5
         elif self.jumpvar == 0:
             self.velocityDown -= 1
         if self.jump and self.jumpvar > 0:
-            if not self.collide(self.playerX, self.playerY - (self.jumpvar ** 2) * 0.17):
-                self.playerY -= (self.jumpvar ** 2) * 0.17
+            if not self.collide(
+                self.playerX, self.playerY - (self.jumpvar ** 2) * 0.17
+            ):
+                self.playerY -= (self.jumpvar ** 2) * 0.12
             else:
                 self.jumpvar = 0
                 self.jump = False
@@ -335,17 +336,17 @@ class mainClass():
         drawOnScreen(
             picture,
             self.playerX - self.scroll[0] - 17,
-            self.playerY - self.scroll[1] - 11
+            self.playerY - self.scroll[1] - 11,
         )
 
     def getPlayerImage(self):
         # Drawing the player to the screen
         if self.direction[0]:
-            self.drawPlayer(self.vitaRunRight[self.runAnimation // 5])
+            self.drawPlayer(vitaRunRight[self.runAnimation // 5])
         elif self.direction[1]:
-            self.drawPlayer(self.vitaRunLeft[self.runAnimation // 5])
+            self.drawPlayer(vitaRunLeft[self.runAnimation // 5])
         else:
-            self.drawPlayer(self.vitaIdle[self.idleAnimation // 10])
+            self.drawPlayer(vitaIdle[self.idleAnimation // 10])
         if self.idleAnimation > 48:
             self.idleAnimation = 0
         if self.runAnimation < 24:
@@ -355,11 +356,10 @@ class mainClass():
 
 
 game = mainClass()
-# Game Loop
 
 
 def gameLoop():
-    while (1):
+    while 1:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
